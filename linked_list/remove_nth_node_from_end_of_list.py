198b4a383f51c8c6e86a121b3d5a15715541b007
Floyd-Droid <jourdonfloyd@gmail.com>
2022-09-05 11:54:59 -0400

    Add remove nth node from end of linked list

--- linked_list/remove_nth_node_from_end_of_list.py
+++ linked_list/remove_nth_node_from_end_of_list.py
@@ -7,13 +7,22 @@ Given the head of a linked list, remove the nth node from the end of the list
 and return its head.
 
 Category: 
-Linked list, recursion
+Linked list, two pointers
 
 Solution Explanation:
+Two pointers approach - To establish the nth node from the end of the list, 
+start at the head and move forward n times or until the current_node's next 
+is None. If we reach a next of None before repeating the above process n-1 
+times, then n must be the length of the list, so we set the head to be the 
+current head's next and return it. Otherwise, we then repeatedly advance the 
+current_node and the lag_node by one each time until the current_node's next 
+is None and we've reached the end of the list. At this point, the lag node's 
+next is the node which needs to be removed. So we store the node after it in 
+memory, set the removal node's next to be None, then set the lag node's next 
+to be the node in memory.
 
-
-TC: 
-SC: 
+TC: O(n)
+SC: O(1)
 """
 
 class ListNode(object):
@@ -37,24 +46,27 @@ class Solution:
 		while k < n and current_node.next is not None:
 			current_node = current_node.next
 			k += 1
-
+		
+		if k < n:
+			head = head.next
+			lag_node.next = None
+			self.print_result(head)
+			return head
 
 		while current_node.next is not None:
 			current_node = current_node.next
 			lag_node = lag_node.next
 		
 		if lag_node.next is current_node:
-			if n == 1:
-				lag_node.next = None
-				head = lag_node
-			elif n == 2:
-				lag_node.next = None
-				head = current_node
+			lag_node.next = None
 		else:
+			tmp_node = lag_node.next.next
 			lag_node.next.next = None
-			lag_node.next = current_node
+			lag_node.next = tmp_node
 
 		self.print_result(head)
+
+		return head
 	
 	def print_result(self, head):
 		current_node = head
@@ -68,11 +80,12 @@ class Solution:
 
 
 if __name__ == '__main__':
+	 # input: [1, 2, 3, 4, 5], n=2   output: [1, 2, 3, 5]
 	node15 = ListNode(5)
 	node14 = ListNode(4, node15)
 	node13 = ListNode(3, node14)
 	node12 = ListNode(2, node13)
-	node11 = ListNode(1, node12)  # input: [1, 2, 3, 4, 5], n=2   output: [1, 2, 3, 5]
+	node11 = ListNode(1, node12)
 
 	# input: [1, 2]. For n=1, output is [1], For n=2, output is [2]
 	node22 = ListNode(2)
@@ -84,4 +97,4 @@ if __name__ == '__main__':
 	node31 = ListNode(1, node32)
 
 	sol = Solution()
-	result = sol.remove_nth_from_end(node21, 1)
+	result = sol.remove_nth_from_end(node31, 3)
